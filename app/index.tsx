c41c0b872ca2219ca20b81f75441989b15fc9849
Angel Iglesias <angel.iglesiaspaez@edu.sait.ca>
2026-02-03 20:12:44 -0700

    added a new component that shows the phone banner on top with the time, signal, battery icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { View } from "react-native";
 import Header from "./components/header";
+import Band from "./components/iphoneUI";
 
 export default function Index() {
   return (
-    <View>
+    <View style={{ backgroundColor: "black" }}>
+      <Band />
       <Header />
     </View>
   );
