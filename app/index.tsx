187cae81a112156fdaaeeb429165adbd7263c2e4
Naila <naipaugue@gmail.com>
2026-02-06 12:44:17 -0700

    made the footer component and the alert

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import Footer from "./componets/Footer";
+
+
+export default function Index() {
+  return (
+    <View>    
+      <Footer/>
+    </View>
+  );
+}
+
