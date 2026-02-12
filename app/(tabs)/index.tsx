3b76fbd55ca875fe1037a12f63e89c7d2ec6fd32
Angel Iglesias <angel.iglesiaspaez@edu.sait.ca>
2026-02-11 22:03:18 -0700

    finished Home body and styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,9 +6,10 @@ export default function Home() {
   return (
     <SafeAreaView style={styles.screen} edges={["top", "bottom"]}>
       <Band />
+      {/* header component */}
       <View style={styles.header}>
         <Text style={styles.leftCornerText}> Home </Text>
-        <Text style={styles.headerBottomText}>Top Picks For You </Text>
+        <Text style={styles.subheaderText}>Top Picks For You </Text>
         <View style={styles.iconContainer}>
           <Image
             source={require("../../assets/images/avatar.png")}
@@ -16,8 +17,78 @@ export default function Home() {
           />
         </View>
       </View>
-
-      <View style={styles.bodyHome}></View>
+      {/* Body component top*/}
+      <SafeAreaView style={styles.bodyHome}>
+        <View>
+          <Text style={styles.smlText}>Made for You</Text>
+          <Image
+            source={require("@/assets/images/homeCard1.png")}
+            style={styles.imgTopBody}
+          />
+        </View>
+        <View>
+          <Text style={styles.smlText}>Listen Again</Text>
+          <Image
+            source={require("@/assets/images/homeCard2.png")}
+            style={styles.imgTopBody}
+          />
+        </View>
+      </SafeAreaView>
+      {/* Body component bottom*/}
+      <Text style={styles.RecentlyPlayedText}>Recently Played {">"}</Text>
+      <SafeAreaView style={styles.bodyHome}>
+        <View>
+          <Image
+            source={require("@/assets/images/album1.png")}
+            style={styles.imgBottomBody}
+          />
+          <Text style={styles.albumText}>Best of Queen</Text>
+          <Text style={styles.smlText}>80&apos;s Classics</Text>
+        </View>
+        <View>
+          <Image
+            source={require("@/assets/images/album2.png")}
+            style={styles.imgBottomBody}
+          />
+          <Text style={styles.albumText}>Frank Sinatra unleashed</Text>
+          <Text style={styles.smlText}>I did it my way</Text>
+        </View>
+        <View>
+          <Image
+            source={require("@/assets/images/album3.png")}
+            style={styles.imgBottomBody}
+          />
+          <Text style={styles.albumText}>Oingo Boingo Brothars</Text>
+          <Text style={styles.smlText}>Zetai 100%!</Text>
+        </View>
+      </SafeAreaView>
+      <Text style={styles.RecentlyPlayedText}>Explore New Genres {">"}</Text>
+      <SafeAreaView style={styles.bodyHome}>
+        <View>
+          <Image
+            source={require("@/assets/images/album1.png")}
+            style={styles.imgBottomBody}
+          />
+          <Text style={styles.albumText}>Best of Queen</Text>
+          <Text style={styles.smlText}>80&apos;s Classics</Text>
+        </View>
+        <View>
+          <Image
+            source={require("@/assets/images/album2.png")}
+            style={styles.imgBottomBody}
+          />
+          <Text style={styles.albumText}>Frank Sinatra unleashed</Text>
+          <Text style={styles.smlText}>I did it my way</Text>
+        </View>
+        <View>
+          <Image
+            source={require("@/assets/images/album3.png")}
+            style={styles.imgBottomBody}
+          />
+          <Text style={styles.albumText}>Oingo Boingo Brothars</Text>
+          <Text style={styles.smlText}>Zetai 100%!</Text>
+        </View>
+      </SafeAreaView>
     </SafeAreaView>
   );
 }
@@ -26,6 +97,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     backgroundColor: "black",
+    overflow: "hidden",
   },
   header: {
     position: "relative",
@@ -39,16 +111,17 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "500",
     position: "absolute",
-    top: 15,
-    left: 6,
+    marginTop: 20,
+    marginLeft: 5,
   },
-  headerBottomText: {
+  subheaderText: {
     color: "#fcfbfb",
     fontSize: 20,
     fontWeight: "500",
     position: "absolute",
-    bottom: 0,
-    left: 15,
+    bottom: -3,
+    marginLeft: 15,
+    marginTop: 5,
   },
   iconContainer: {
     position: "absolute",
@@ -58,18 +131,56 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   avatarIcon: {
-    top: 5,
-    width: 30,
-    height: 30,
+    width: 35,
+    height: 35,
     borderRadius: 25,
     resizeMode: "contain",
+    marginTop: 13,
   },
   bodyHome: {
-    flex: 1,
+    flexGrow: 0,
     flexDirection: "row",
-    flexWrap: "wrap",
+    flexWrap: "nowrap",
     backgroundColor: "black",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    marginTop: 5,
+    marginLeft: 5,
+    paddingHorizontal: 4,
+    gap: 5,
+  },
+  imgTopBody: {
+    width: 230,
+    height: 300,
+    borderRadius: 10,
+    marginTop: 3,
+    resizeMode: "contain",
+    marginLeft: 2,
+  },
+  smlText: {
+    color: "white",
+    opacity: 0.5,
+    marginBottom: 3,
+    marginLeft: 8,
+    marginTop: 3,
+  },
+  RecentlyPlayedText: {
+    marginTop: 20,
+    marginLeft: 15,
+    color: "#fcfbfb",
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  imgBottomBody: {
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+    marginTop: 3,
+    marginLeft: 5,
+  },
+  albumText: {
+    color: "white",
+    marginTop: 3,
+    marginLeft: 8,
   },
 });
